c4f26b7e41d000dfa46db21eb7f7f5a59d7baf53
Navneet <navneet23341@iiitd.ac.in>
2025-05-18 22:36:17 +0530

    user syystem

--- storee/models.py
+++ storee/models.py
@@ -2,6 +2,7 @@ from django.db import models
 
 # Create your models here.
 class Products(models.Model):
+    productName= models.CharField(max_length= 30 , null= True)
     productId = models.CharField(max_length=5)
     price = models.CharField(max_length=15)
     #image = 
