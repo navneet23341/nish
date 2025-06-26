fbc62483287e4da1886b0eb8bee8b0731451381c
Navneet <navneet23341@iiitd.ac.in>
2025-06-27 03:32:25 +0530

    header

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,14 +2,13 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Page from './frontpage'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <div className="bg-blue-500 text-white p-6 rounded-xl text-center">
-      Tailwind is working bro! 🎉
-    </div>
+    <Page/>
   )
 }
 
---
+++ frontend/src/frontpage.jsx
@@ -0,0 +1,14 @@
+function Page(){
+    return( <header class="bg-amber-100 w-full">
+        <div class="absolute left-0 inline bg-amber-100" ><h2>Geeee & Nav</h2></div>
+        <div class="inline-block flex justify-center bg-amber-100 w-full"><nav>
+            <a href="">product</a>
+            <a href="">contact us</a>
+            <a href="">contact us</a>
+            <a href="">contact us</a>
+            <a href="">contact us</a>
+            </nav></div>
+    </header>
+    )
+}
+export default Page
