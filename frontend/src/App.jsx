fa8c048fa55b5628ec706a09d674b71bf62eb4f9
Navneet <navneet23341@iiitd.ac.in>
2025-07-01 16:47:01 +0530

    minor changes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 
 import Page from './frontpage';
 import ShoppingList from './shoppingList';
@@ -14,13 +14,13 @@ function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<Page />} />
         <Route path="/shop" element={<ShoppingList />} />
         <Route path="/product/1" element={<ProductDetail />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
